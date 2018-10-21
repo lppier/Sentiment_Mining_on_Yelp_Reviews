cb2c10ea37be108a90cb126ae0dbf3bb4e5c16e0
David Leonardi <davidleonardi418@gmail.com>
2018-10-21 21:20:28 +0800

    Try to break Yelp strong defence. Phew!!

--- crawling/YelpCrawler.py
+++ crawling/YelpCrawler.py
@@ -15,6 +15,8 @@ BASE_URL = "https://www.yelp.com"
 DATA_FILE = "data/yelp_restaurant_top10.json"
 PAGE_SIZE = 20
 REVIEW_LIMIT = 10
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+RETRY_ATTEMPTS = 5
 
 RESTAURANT_CATEGORIES = {
     "chinese": {
@@ -85,125 +87,146 @@ def string_to_isoformatdate(datestring):
 
 
 def currency_to_float(currency_string):
-    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
     return 0.0 if currency_string == "-" else locale.atof(currency_string.strip("$"))
 
 
 def string_to_integer(string):
-    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
     return 0 if string == "-" else locale.atoi(string)
 
 
 def string_to_float(string):
-    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
     return 0.0 if string == "-" else locale.atof(string)
 
 
 def get_string(element, css_selector):
-    data_element = element.select_one(css_selector)
-    return data_element.get_text(separator="\n").strip().replace(u'\xa0', u' ')
+    try:
+        data_element = element.select_one(css_selector)
+        return data_element.get_text(separator="\n").strip().replace(u'\xa0', u' ')
+    except AttributeError:
+        logger.error("element: {} is not exists".format(css_selector))
+        return ""
 
 
 def get_rating(element, css_selector):
-    data_element = element.select_one(css_selector)
-    rating = data_element.attrs["title"][0]
-    return string_to_integer(rating)
+    try:
+        data_element = element.select_one(css_selector)
+        rating = data_element.attrs["title"][0]
+        return string_to_integer(rating)
+    except AttributeError:
+        logger.error("element: {} is not exists".format(css_selector))
+        return 0
 
 
 def get_date(element, css_selector):
-    data_element = element.select_one(css_selector)
+    try:
+        data_element = element.select_one(css_selector)
 
-    # remove unwanted element (Updated Review)
-    unwanted_element = data_element.select_one("small.bullet-before.has-archived-review")
-    if unwanted_element:
-        unwanted_element.decompose()
+        # remove unwanted element (Updated Review)
+        unwanted_element = data_element.select_one("small.bullet-before.has-archived-review")
+        if unwanted_element:
+            unwanted_element.decompose()
 
-    return string_to_isoformatdate(data_element.get_text().strip())
+        return string_to_isoformatdate(data_element.get_text().strip())
+    except Exception:
+        logger.error(traceback.print_exc())
+        return ""
 
 
-def fetch_restaurant_reviews(url, id, name, category, save_data=False):
+
+def fetch_restaurant_reviews(category, index, url, id, name, save_data=False):
     
-    page_no = 0
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    reviews_element = soup.select("div.review--with-sidebar")
-    reviews_count = len(reviews_element)
+    attempt = 0
+    reviews_found = False
+    while attempt < RETRY_ATTEMPTS and not reviews_found:
+        attempt += 1
+        logger.info("Attempt No {}".format(attempt))
 
-    records = {}
-
-    while reviews_count > 1:
-        
         # add random sleep to prevent robot blocking
-        time.sleep(random.randint(0, 2))
-
-        records[str(page_no * PAGE_SIZE)] = {
-            "id":  id,
-            "name": name,
-            "category": category,
-            "servesCuisine": soup.select_one("span.category-str-list > a").get_text().strip(),
-            "priceRange": soup.select_one("dd.nowrap.price-description").get_text().strip(),
-            "address": {
-                "addressLocality": "United Kingdom",
-                "addressRegion": soup.select_one("div.map-box-address > span.neighborhood-str-list").get_text().strip(),
-                "streetAddress": soup.select_one("div.map-box-address > strong.street-address > address").get_text(separator="\n").strip(),
-                "postalCode": None,
-                "addressCountry": "GB"
-            },
-            "context": "http://schema.org/",
-            "image": soup.select_one("img.photo-box-img").attrs["src"],
-            "type": "Restaurant",
-            "telephone": soup.select_one("span.biz-phone").get_text().strip(),
-            "review": []
-        }    
-        reviews = []
-        logger.info("Fetch reviews from page {}".format(page_no + 1))
-        for element in reviews_element:
-            try:
-                if element.attrs["data-review-id"]:
-                    review = {
-                        "reviewId": element.attrs["data-review-id"]
-                    }
-                    for aspect in CRAWL_TEMPLATE_DICT:
-                        element_attrib = CRAWL_TEMPLATE_DICT[aspect]
-                        if element_attrib["type"] == "string":
-                            review[aspect] = get_string(element, element_attrib["css_selector"])
-                        if element_attrib["type"] == "date":
-                            review[aspect] = get_date(element, element_attrib["css_selector"])
-                        if element_attrib["type"] == "rating":
-                            rating = get_rating(element, element_attrib["css_selector"])
-                            review[aspect] = {
-                                "ratingValue": rating
-                            }
-                            review["sentiment"] = SENTIMENT_MAP[rating]
-                    reviews.append(review)
-            except KeyError as err:
-                logger.error(traceback.print_exc())
-                pass
-
-        records[str(page_no * PAGE_SIZE)]["review"] = reviews
-        
-        page_no += 1
-        page = requests.get(url + "&start={}".format(page_no * PAGE_SIZE))
+        time.sleep(random.randint(6, 20))
+            
+        page_no = 0
+        page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
         reviews_element = soup.select("div.review--with-sidebar")
         reviews_count = len(reviews_element)
+        reviews_found = (reviews_count >= 1)
 
-    logger.info("---------------------------------------")
-    
-    if save_data:
-        with open("./data/yelp_{}_{}.json".format(category, name), "w") as outfile:
-            json.dump(records, fp=outfile, indent=4)
+        records = {}
+        while reviews_count > 1:
+
+            # add random sleep to prevent robot blocking
+            time.sleep(random.randint(2, 5))
+
+            records[str(page_no * PAGE_SIZE)] = {
+                "id":  id,
+                "name": name,
+                "category": category,
+                "servesCuisine": get_string(soup, "span.category-str-list > a"),
+                "priceRange": get_string(soup, "dd.nowrap.price-description"),
+                "address": {
+                    "addressLocality": "United Kingdom",
+                    "addressRegion": get_string(soup, "div.map-box-address > span.neighborhood-str-list"),
+                    "streetAddress": get_string(soup, "div.map-box-address > strong.street-address > address"),
+                    "postalCode": None,
+                    "addressCountry": "GB"
+                },
+                "context": "http://schema.org/",
+                "image": soup.select_one("img.photo-box-img").attrs["src"],
+                "type": "Restaurant",
+                "telephone": get_string(soup, "span.biz-phone"),
+                "review": []
+            }    
+            reviews = []
+            logger.info("Fetch reviews from page {}".format(page_no + 1))
+            for element in reviews_element:
+                try:
+                    if element.attrs["data-review-id"]:
+                        review = {
+                            "reviewId": element.attrs["data-review-id"]
+                        }
+                        for aspect in CRAWL_TEMPLATE_DICT:
+                            element_attrib = CRAWL_TEMPLATE_DICT[aspect]
+                            if element_attrib["type"] == "string":
+                                review[aspect] = get_string(element, element_attrib["css_selector"])
+                            if element_attrib["type"] == "date":
+                                review[aspect] = get_date(element, element_attrib["css_selector"])
+                            if element_attrib["type"] == "rating":
+                                rating = get_rating(element, element_attrib["css_selector"])
+                                review[aspect] = {
+                                    "ratingValue": rating
+                                }
+                                review["sentiment"] = SENTIMENT_MAP[rating]
+                        reviews.append(review)
+                except KeyError:
+                    logger.error(traceback.print_exc())
+                    pass
+
+            records[str(page_no * PAGE_SIZE)]["review"] = reviews
+            
+            page_no += 1
+            page = requests.get(url + "&start={}".format(page_no * PAGE_SIZE))
+            soup = BeautifulSoup(page.content, 'html.parser')
+            reviews_element = soup.select("div.review--with-sidebar")
+            reviews_count = len(reviews_element)
+
+        logger.info("---------------------------------------")
+        
+        if save_data:
+            with open("./data/yelp_{}_{}_{}.json".format(category, index, name), "w") as outfile:
+                json.dump(records, fp=outfile, indent=4, ensure_ascii=False)
 
     return records
 
 
 def fetch_all_restaurants_reviews(category, url, limit=4):
     logger.info("=======================================")
-    logger.info("Fetch all {} restaurants reviews".format(category))
+    logger.info("Fetch all {} restaurants".format(category))
     logger.info("=======================================")
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     elements = soup.select("div.search-result")
+
+    logger.info("restaurant count: {}".format(len(elements)))
 
     index = 0
     for element in elements:
@@ -215,7 +238,7 @@ def fetch_all_restaurants_reviews(category, url, limit=4):
         restaurant_name = restaurant_element.text
 
         logger.info("Fetch reviews of {}".format(restaurant_name))
-        fetch_restaurant_reviews(restaurant_link, restaurant_id, restaurant_name, category, save_data=True)
+        fetch_restaurant_reviews(category, index, restaurant_link, restaurant_id, restaurant_name, save_data=True)
         index += 1
 
 
