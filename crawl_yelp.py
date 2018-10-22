b3791ddadd0f24a79882304b6245a561a31e6d9a
Pier <madstrum@gmail.com>
2018-10-22 11:20:37 +0800

    local

--- crawl_yelp.py
+++ crawl_yelp.py
@@ -5,15 +5,15 @@ from crawler.YelpCrawler import fetch_all_restaurants_reviews
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 RESTAURANT_CATEGORIES = {
-    "chinese": {
-        "url": "https://www.yelp.com/search?find_desc=Restaurants&find_loc=London&start=0&sortby=review_count&cflt=chinese"
-    },
-    "japanese": {
-        "url": "https://www.yelp.com/search?find_desc=Restaurants&find_loc=London&start=0&sortby=review_count&cflt=japanese"
-    },
-    "indian": {
-        "url": "https://www.yelp.com/search?find_desc=Restaurants&find_loc=London&start=0&sortby=review_count&cflt=indian"
-    },
+    # "chinese": {
+    #     "url": "https://www.yelp.com/search?find_desc=Restaurants&find_loc=London&start=0&sortby=review_count&cflt=chinese"
+    # },
+    # "japanese": {
+    #     "url": "https://www.yelp.com/search?find_desc=Restaurants&find_loc=London&start=0&sortby=review_count&cflt=japanese"
+    # },
+    # "indian": {
+    #     "url": "https://www.yelp.com/search?find_desc=Restaurants&find_loc=London&start=0&sortby=review_count&cflt=indian"
+    # },
     "french": {
         "url": "https://www.yelp.com/search?find_desc=Restaurants&find_loc=London&start=0&sortby=review_count&cflt=french"
     },
